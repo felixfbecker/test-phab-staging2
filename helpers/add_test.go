7906bef915c00b14baa8194a63cffee56e21d4aa
ijsnow <isaacjsnow@gmail.com>
2018-01-09 12:41:28 -0800

    add go/scanner
    Differential Revision: http://phabricator.sgdev.org/D14

--- helpers/add_test.go
+++ helpers/add_test.go
@@ -8,10 +8,3 @@ func TestAdd(t *testing.T) {
 		t.Errorf("got %d, expected %d", sum, 6)
 	}
 }
-
-func TestSubtract(t *testing.T) {
-	diff := Subtract(1, 2, 3)
-	if diff != -4 {
-		t.Errorf("got %d, expected %d", diff, -4)
-	}
-}
